3c7e2f43a9940a35755ab7cb80bca8b1ac4f42a6
ESWZY <1006460505@qq.com>
2020-03-19 00:25:37 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,31 +1,50 @@
 # webvpn-dlut
 
-```webvpn.dlut.edu.cn```是DUT的WebVPN，提供了从校外访问校园网环境资源的接口，可供访问校内资源。
+```webvpn.dlut.edu.cn```��DUT��WebVPN���ṩ�˴�У�����У԰��������Դ�Ľӿڣ��ɹ�����У����Դ��
 
-但是登录后，由于链接为加密状态，仅能访问该系统门户所提供的站点。
+���ǵ�¼����������Ϊ����״̬�����ܷ��ʸ�ϵͳ�Ż����ṩ��վ�㡣
 
-正巧最近的课程涉及到该链接编制方式，用Python实现加密链接与普通链接的转换方式。
+��������Ŀγ��漰�������ӱ��Ʒ�ʽ����Pythonʵ�ּ�����������ͨ���ӵ�ת����ʽ��
 
-经查，该系统为```网瑞达科技```的资源访问控制系统（WebVPN），并使用了```wrdvpnisthebest!```作为key和iv。
+���飬��ϵͳΪ[�����Ƽ�](https://www.wrdtech.com/content/content.php?p=2_30_203)����Դ���ʿ���ϵͳ��WebVPN������ʹ����```wrdvpnisthebest!```��Ϊkey��iv��
 
-可能所有使用该系统的WebVPN都可使用同种方式实现链接转换，可供一试。
+���ܷ�����*AES-128-CFB*
 
-# 代码结构
-源代码为Python文件：“webvpn.py”
+# ����ṹ
+Դ����ΪPython�ļ�����webvpn.py��
 
-代码分为四个函数：
+�����Ϊ�ĸ�������
 
 ```python
-        getCiphertext(plaintext, key = key_, cfb_iv = iv_, size = 128) #利用明文生成密文
-        getPlaintext(ciphertext, key = key_, cfb_iv = iv_, size = 128) #利用密文得到明文
-        getVPNUrl(url)        #从普通的url转换成webvpn的url
-        getOrdinaryUrl(url)   #从webvpn的url反推回普通的url
+        getCiphertext(plaintext, key = key_, cfb_iv = iv_, size = 128) #����������������
+        getPlaintext(ciphertext, key = key_, cfb_iv = iv_, size = 128) #�������ĵõ�����
+        getVPNUrl(url)        #����ͨ��urlת����webvpn��url
+        getOrdinaryUrl(url)   #��webvpn��url���ƻ���ͨ��url
 ```
 
-# 运行方式
+# ���з�ʽ
 
-如果缺少相关Python库，在命令行执行
-```pip install pycryptodome```
+���ȱ�����Python�⣬��������ִ��
+```pip install -r requirements.txt```
 
-并直接运行“webvpn.py” 可以得到“xueshu.baidu.com”的密文、“e7e056d2253161546b468aa395”对应的明文、一个知网论文网页的webvpn网址和一个webvpn网址对应的原先的网址。
+��ֱ������ ```python webvpn.py``` 
 
+���Եõ�һ��֪��������ҳ��webvpn��ַ��һ��webvpn��ַ��Ӧ��ԭ�ȵ���ַ��
+
+# ������
+
+��������˵������Ӧ�õ�����ʹ�ø�ϵͳ��WebVPN�ϡ��ڴ��ṩһ����ֲ��˼·��
+
+��
+
+```python
+institution = 'webvpn.dlut.edu.cn'
+```
+
+��Ϊ
+
+```python
+institution = 'webvpn.xxx.edu.cn'
+```
+
+�����л�ΪXXX��WebVPN��GL
--- webvpn.py
+++ webvpn.py
@@ -3,13 +3,14 @@ from binascii import hexlify, unhexlify
 
 key_ = b'wrdvpnisthebest!'
 iv_  = b'wrdvpnisthebest!'
+institution = 'webvpn.dlut.edu.cn'   # Change the hostname here like 'webvpn.xxx.edu.cn'
 
 def getCiphertext(plaintext, key = key_, cfb_iv = iv_, size = 128):
     '''From plantext hostname to ciphertext'''
     
     message = plaintext.encode('utf-8')
     
-    cfb_cipher_encrypt = AES.new(key, AES.MODE_CFB, cfb_iv, segment_size = size)       #must include segment_size
+    cfb_cipher_encrypt = AES.new(key, AES.MODE_CFB, cfb_iv, segment_size = size)       # Must include segment_size
     mid = cfb_cipher_encrypt.encrypt(message)
 
     return hexlify(mid).decode()
@@ -40,7 +41,7 @@ def getVPNUrl(url):
 
     key = hexlify(iv_).decode('utf-8')
     
-    return 'https://webvpn.dlut.edu.cn/' + pro + '/' + key + cph + '/' + fold
+    return 'https://' + institution + '/' + pro + '/' + key + cph + '/' + fold
 
 def getOrdinaryUrl(url):
     '''From webVPN url to ordinary url'''
@@ -59,13 +60,13 @@ def getOrdinaryUrl(url):
         return pro + "://" + hostname + '/' + fold
 
 if __name__ == '__main__':
-    print(getCiphertext('xueshu.baidu.com'))
-    print(getPlaintext('e7e056d2253161546b468aa395'))
+    #print(getCiphertext('xueshu.baidu.com'))
+    #print(getPlaintext('e7e056d2253161546b468aa395'))
 
     url = 'https://kns.cnki.net/KCMS/detail/detail.aspx?dbcode=CJFQ&dbname=CJFD2007&filename=JEXK200702000&uid=WEEvREcwSlJHSldRa1FhcTdnTnhXY20wTWhLQWVGdmJFOTcvMFFDWDBycz0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&v=MTYzNjU3cWZaT2RuRkNuaFZMN0tMeWpUWmJHNEh0Yk1yWTlGWklSOGVYMUx1eFlTN0RoMVQzcVRyV00xRnJDVVI='
-    print('\nFrom ordinary url: \n' + getVPNUrl(url))
+    print('From ordinary url: \n' + getVPNUrl(url))
 
-    VPNUrl = 'https://webvpn.dlut.edu.cn/https/77726476706e69737468656265737421fbf952d2243e635930068cb8/KCMS/detail/detail.aspx?dbcode=CJFQ&dbname=CJFD2007&filename=JEXK200702000&uid=WEEvREcwSlJHSldRa1FhcTdnTnhXY20wTWhLQWVGdmJFOTcvMFFDWDBycz0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&v=MTYzNjU3cWZaT2RuRkNuaFZMN0tMeWpUWmJHNEh0Yk1yWTlGWklSOGVYMUx1eFlTN0RoMVQzcVRyV00xRnJDVVI='
+    VPNUrl = 'https://' + institution + '/https/77726476706e69737468656265737421fbf952d2243e635930068cb8/KCMS/detail/detail.aspx?dbcode=CJFQ&dbname=CJFD2007&filename=JEXK200702000&uid=WEEvREcwSlJHSldRa1FhcTdnTnhXY20wTWhLQWVGdmJFOTcvMFFDWDBycz0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&v=MTYzNjU3cWZaT2RuRkNuaFZMN0tMeWpUWmJHNEh0Yk1yWTlGWklSOGVYMUx1eFlTN0RoMVQzcVRyV00xRnJDVVI='
     print('\nFrom webVPN url: \n' + getOrdinaryUrl(VPNUrl))
 
     
